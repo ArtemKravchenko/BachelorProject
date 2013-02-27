341eb27b51fc210f4d5f4c640e181e867fb385eb
Artem Kravchenko <artemkravchenko1991@gmail.com>
2013-02-27 18:27:03 +0200

    no message

---
+++ Vocational Guidance/Custom Controls/VGTableView.h
@@ -0,0 +1,13 @@
+//
+//  VGTableView.h
+//  Vocational Guidance
+//
+//  Created by Artem Kravchenko on 27.02.13.
+//  Copyright (c) 2013 K304. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VGTableView : UIScrollView
+
+@end
---
+++ Vocational Guidance/Custom Controls/VGTableView.m
@@ -0,0 +1,31 @@
+//
+//  VGTableView.m
+//  Vocational Guidance
+//
+//  Created by Artem Kravchenko on 27.02.13.
+//  Copyright (c) 2013 K304. All rights reserved.
+//
+
+#import "VGTableView.h"
+
+@implementation VGTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
